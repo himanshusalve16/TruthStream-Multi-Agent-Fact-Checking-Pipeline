1bad07b1323d683a308a3aa871ea0dad21abdc9e
himanshusalve16 <148865395+himanshusalve16@users.noreply.github.com>
2026-06-10 23:14:23 +0530

    Backend Deployment Fix

--- backend/src/main/java/com/truthstream/TruthStreamApplication.java
+++ backend/src/main/java/com/truthstream/TruthStreamApplication.java
@@ -4,14 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EnableDiscoveryClient
+
 public class TruthStreamApplication {
     private static final Logger logger = LoggerFactory.getLogger(TruthStreamApplication.class);
 
